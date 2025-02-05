187103563792e0b9a64f1d6861834633803408ac
Zhansultan <111305698+zhans1@users.noreply.github.com>
2025-02-05 18:55:21 +0500

    Update config.py

--- config.py
+++ config.py
@@ -1,11 +1,11 @@
 import os
 
 class Config:
-    TELEGRAM_BOT_TOKEN = '6931484907:AAHkSpe01TCywyqDT7ehej2HUMmVk0l-N2I'
+    TELEGRAM_BOT_TOKEN = 'TOKEN'
     DB_HOST = 'localhost'
     DB_NAME = 'telegram_stickers'
     DB_USER = 'postgres'
-    DB_PASSWORD = '123123'
+    DB_PASSWORD = 'PASS'
     
     # Local development settings
     WEBHOOK_HOST = 'http://localhost:5000'  # Changed to localhost
